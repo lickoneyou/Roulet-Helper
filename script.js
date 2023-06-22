1a077d696baf15a8c81710865a868a7d8ad85f8c
Kiryll Khomchanka <dickdurex265@gmail.com>
2023-06-22 16:11:02 +0300

    fixed

--- script.js
+++ script.js
@@ -5,22 +5,29 @@ const outAmount = document.querySelector('.out-amount')
 function calculation() {
   let amount = +document.querySelector('.amount').value
   let oneRulet = +document.querySelector('.oneRulet').value
+  const probabilityOut = document.querySelector('.probability-out')
+  probabilityOut.innerHTML = 'Probability: <br>'
   let arr = []
   let probability = 0.49292929
 
-  while (arr.reduce((acc, el) => acc + el, 0) + oneRulet <= amount) {
+  while (
+    arr.reduce((acc, el) => acc + el, 0) + oneRulet <= amount &&
+    amount &&
+    oneRulet
+  ) {
     arr.push(oneRulet)
     oneRulet *= 2
   }
 
   let probCount = 1
 
-  while (probCount < arr.length) {
+  while (probCount <= 10 && arr.length > 0) {
+    probabilityOut.innerHTML += `<br>${probCount}: ${probability} <br>`
     probability /= 2
     probCount++
   }
 
-  spin.innerHTML = `Spin: ${arr.length} <br> Probability: ${probability}`
+  spin.innerHTML = `Spin: ${arr.length}`
   cost.innerHTML = `Cost: ${arr.join(' -> ')}`
   outAmount.textContent = `Amount: ${arr.reduce((acc, el) => acc + el, 0)}`
 }
